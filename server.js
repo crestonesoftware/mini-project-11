778aaa900ea8c235a8bb22ce6b385e3361a70c36
cuhrmunken <elliott.davis@crestonetech.com>
2024-06-28 08:46:17 -0700

    adds wildcard route

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const path = require('path');
-const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
+const express = require("express");
+const path = require("path");
+const { clog } = require("./middleware/clog");
+const api = require("./routes/index.js");
+const fs = require("fs");
 
 const PORT = process.env.port || 3001;
 
@@ -13,19 +14,26 @@ app.use(clog);
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+app.use("/api", api);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 // GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+app.get("/feedback", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/pages/feedback.html"))
 );
+
+app.get("*", (req, res) => {
+  console.log(path.join(__dirname, "public/pages"));
+  res
+    .status(404)
+    .sendFile("404.html", { root: path.join(__dirname, "public/pages") });
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
